e4bdd841364903c0c0328f2e6cfbbb9f29779134
mamo1016 <rr0061vv@ed.ritsumei.ac.jp>
2017-11-15 18:44:52 +0900

    Initial Commit

---
+++ XI/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  XI
+//
+//  Created by 上田　護 on 2017/11/15.
+//  Copyright © 2017年 上田　護. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

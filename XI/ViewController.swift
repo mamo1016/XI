70e7e9c027d1b29cf1f897598649b93bbf10f336
AkiraMAEGAWA <maeaki112@gmail.com>
2017-11-16 21:07:54 +0900

    tribial change

--- XI/ViewController.swift
+++ XI/ViewController.swift
@@ -9,7 +9,8 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    let hoge = "poyo"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
